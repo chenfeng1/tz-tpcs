4834a31f1abccbf43663e1eed0d6cb2abc7f8730
usedtolove <usedtolove@gmail.com>
2015-01-23 17:47:20 +0800

    准备测试类

---
+++ trunk/src/test/java/com/tz/tpcs/xml/Mobile.java
@@ -0,0 +1,32 @@
+package com.tz.tpcs.xml;
+
+public class Mobile {
+
+    private String name;
+    private String model;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
